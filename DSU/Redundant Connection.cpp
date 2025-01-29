6c069f366b5b81ad9ad9926ec0077373d8ea1d55
Ankesh-Hatui <155291507+Ankesh-Hatui@users.noreply.github.com>
2025-01-29 22:22:33 +0530

    Update Redundant Connection.cpp

--- DSU/Redundant Connection.cpp
+++ DSU/Redundant Connection.cpp
@@ -54,3 +54,62 @@ public:
         return ans;
     }
 };
+
+//Approach-3 (DSU - Union Find with Path compression)
+//T.C : O(n * alpha(n))
+//S.C : O(n)
+
+class Solution {
+    int find(vector<int>&parent,int u){
+        if (parent[u]!=u){
+            parent[u]=find(parent,parent[u]);
+        }
+        return parent[u];
+    }
+    void uni(vector<int>&parent,vector<int>&rank,int u,int v){
+        int par_u=find(parent,u);
+        int par_v=find(parent,v);
+
+        if (par_u==par_v){
+            return;
+        }
+        if (rank[par_u]>rank[par_v]){
+            parent[par_v]=par_u;
+        }
+        else if (rank[par_v]>rank[par_u]){
+            parent[par_u]=par_v;
+        }
+        else{
+            parent[par_v]=par_u;
+            rank[par_u]++;
+        }
+    }
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n=edges.size();
+        vector<int>parent(n+1),rank(n+1,0);
+        vector<int>ans;
+        for (int i=0;i<=n;i++){
+            parent[i]=i;
+        }
+
+        for (int i=0;i<n;i++){
+            int u=edges[i][0];
+            int v=edges[i][1];
+
+            int par_u=find(parent,u);
+            int par_v=find(parent,v);
+
+            if (par_u==par_v){
+                ans={u,v};
+                break;
+            }
+            else{
+                uni(parent,rank,u,v);
+            }
+        }
+
+        return ans;
+
+    }
+};
